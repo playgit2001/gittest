0b8e1c8b79bacf73a0fbf78f3211121e0ab26add
playgit2001 <2719296038@qq.com>
2022-07-22 22:32:30 +0800

    leetcode第217题

---
+++ Test217.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+public class Test217 {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i+1<nums.length && nums[i]==nums[i+1])
+            {return true;}
+        }
+        return false;
+    }
+}

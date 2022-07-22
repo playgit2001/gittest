599543c1c8bd1c5c40b6d320c23f73c8daa368dd
playgit2001 <2719296038@qq.com>
2022-07-22 22:32:45 +0800

    leetcode第191题

---
+++ Test191.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+public class Test191 {
+    public int hammingWeight(int n) {
+        int res=0;
+        while(n!=0)
+        {
+            res+=(n&1);
+            n=n>>>1;
+        }
+        return res;
+    }
+}

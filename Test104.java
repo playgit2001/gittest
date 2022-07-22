50f8d8d2740d5b0f709631c963377566a36a9645
playgit2001 <2719296038@qq.com>
2022-07-22 22:35:38 +0800

    leetcode第104题

---
+++ Test104.java
@@ -0,0 +1,21 @@
+package com.example.test;
+
+public class Test104 {
+    class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
